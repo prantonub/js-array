df882fe73762dea6e55b8b7bc8490be82c2ee1ff
prantonub <prantonub.cse23@gmail.com>
2025-07-26 19:20:29 +0600

    task added

---
+++ problem-10.js
@@ -0,0 +1,12 @@
+/*
+Movie List Management Problem
+
+You are developing a movie watchlist feature.
+
+1. Start with an array containing 5 of your favorite movies.
+2. Add 3 more movies to the end of the list using push().
+3. Then remove the last 2 movies using pop().
+4. Finally:
+   - Print the final movie list
+   - Print the names of the two removed movies
+*/
