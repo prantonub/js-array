e0750034bbd6ccd9acfb8106877c7426c81760db
prantonub <prantonub.cse23@gmail.com>
2025-07-27 23:43:01 +0600

    task added

--- problem-10.js
+++ problem-10.js
@@ -10,3 +10,18 @@ You are developing a movie watchlist feature.
    - Print the final movie list
    - Print the names of the two removed movies
 */
+// Step 1: Initial movie list
+const movies = ['Inception', 'Interstellar', 'The Dark Knight', 'Shutter Island', 'Fight Club'];
+
+// Step 2: Add 3 more movies using push()
+movies.push('The Prestige', 'Memento', 'Parasite');
+
+// Step 3: Remove the last 2 movies using pop()
+const removedMovie1 = movies.pop();
+const removedMovie2 = movies.pop();
+
+// Step 4: Print the final movie list
+console.log("Final Movie List:", movies);
+
+// Step 5: Print the names of the two removed movies
+console.log("Removed Movies:", removedMovie1, "and", removedMovie2);
